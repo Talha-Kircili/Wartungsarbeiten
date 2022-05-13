ccc2f2961bb6fe169ed2b5e1393af538ec66ec5b
whIstl3bl0w3r <85405690+whIstl3bl0w3r@users.noreply.github.com>
2022-05-14 00:20:48 +0200

    Create smartmon.py

---
+++ smartmon.py
@@ -0,0 +1,39 @@
+from multiprocessing import Pool, freeze_support
+from utils import *
+
+
+''' settings '''
+csv_file = "smartmon.csv"   # output file
+table_head = ['PC', 'Zustand', 'Protokoll'] # column names
+defekt_pcs = [(3,2)] # defective systems in list of tuple, example: reihe3-pc2 => (3,2)
+
+
+def main(x,y):
+    host = f"reihe{x}-pc{y}"
+
+    if (x,y) in defekt_pcs:
+        write(csv_file, [host,'ignored','ignored'], 'a')
+        return 
+
+    client = connect(host, 'root', f'{expanduser('~')}/.ssh/netzlabor-root.key')
+    if client == -1:
+        write(csv_file, [host,'Exception','Exception'], 'a')
+        return
+
+    command(client, host, "sudo apt install -y smartmontools")
+    command(client, host, "smartctl -t short -C /dev/sda")
+    protokoll = command(client, host, "smartctl -a /dev/sda")
+    zustand = loads(command(client, host, "smartctl -a /dev/sda -j"))
+
+    client.close()
+
+    zustand = zustand["ata_smart_self_test_log"]["standard"]["table"][-1]["status"]["passed"]
+    print(f"{host}: passed: {zustand}")
+    write(csv_file, [host, zustand, protokoll], 'a')
+
+
+if __name__ == "__main__":
+    freeze_support()
+    write(csv_file, table_head, 'w')
+    with Pool() as p:
+        p.starmap(main, list((x,y) for x in range(1,5) for y in range(1,7)))
