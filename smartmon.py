77e5e023e4c7ce3364dd622e6a78e3015d401f52
whIstl3bl0w3r <85405690+whIstl3bl0w3r@users.noreply.github.com>
2022-05-17 01:13:19 +0200

    Update smartmon.py

--- smartmon.py
+++ smartmon.py
@@ -1,39 +1,54 @@
-from multiprocessing import Pool, ThreadPool
+from multiprocessing.pool import ThreadPool
+from multiprocessing import freeze_support
 from utils import *
 
 
 ''' settings '''
 csv_file = "smartmon.csv"   # output file
 table_head = ['PC', 'Zustand', 'Protokoll'] # column names
-defekt_pcs = [(3,2)] # defekte rechner in diese liste als tuple angeben. bsp.: reihe3-pc2 => (3,2)
+defekt_pcs = [(3,2)]        # defective machines in list in forms of tuple. f.e reihe3-pc2 => (3,2)
 
 
 def main(x,y):
-    host = f"reihe{x}-pc{y}"
-
+    hostname = f"reihe{x}-pc{y}"
+    ''' skip defective machines '''
     if (x,y) in defekt_pcs:
-        write(csv_file, [host,'ignored','ignored'], 'a')
-        return 
-
-    client = connect(host, 'root', f'{expanduser('~')}/.ssh/netzlabor-root.key')
+        return write(csv_file, [hostname,'defekt','defekt'], 'a')
+    ''' establish ssh connection '''
+    client = connect(hostname, 'root')
     if client == -1:
-        write(csv_file, [host,'Exception','Exception'], 'a')
-        return
-
-    command(client, host, "sudo apt install -y smartmontools")
-    command(client, host, "smartctl -t short -C /dev/sda")
-    protokoll = command(client, host, "smartctl -a /dev/sda")
-    zustand = loads(command(client, host, "smartctl -a /dev/sda -j"))
-
+        return write(csv_file, [hostname, 'Host unreachable', 'Host unreachable'], 'a')
+    ''' check/install smartmontools '''
+    apt = command(client, hostname, "apt list -a smartmontools")
+    if 'smartmontools' in apt:
+        print(f"{hostname}: smartmontools is installed\n")
+    else:
+        pkg = command(client, hostname, "sudo apt install -y smartmontools", timeout=10)
+        if pkg == -1:
+            msg = "failed installing smartmontools"
+            print(f"{hostname}: {msg}\n")
+            client.close()
+            return write(csv_file, [hostname, msg, msg], 'a')
+        print(f"{hostname}: smartmontools successfully installed\n")
+    ''' run smartctl short test and get the results '''
+    command(client, hostname, "smartctl -t short -C /dev/sda")
+    protokoll = command(client, hostname, "smartctl -a /dev/sda")
+    zustand = command(client, hostname, "smartctl -a /dev/sda -j")
+    ''' json.loads error handling '''
+    try:
+        zustand = loads(zustand)
+        zustand = zustand["ata_smart_self_test_log"]["standard"]["table"][-1]["status"]["passed"]
+        print(f"{hostname}: passed: {zustand}\n")
+    except Exception as e:
+        zustand = e
+        print(f"{hostname}: {e}\nOutput: {zustand}\n")
+    ''' close connection and document results '''
     client.close()
-
-    zustand = zustand["ata_smart_self_test_log"]["standard"]["table"][-1]["status"]["passed"]
-    print(f"{host}: passed: {zustand}")
-    write(csv_file, [host, zustand, protokoll], 'a')
+    write(csv_file, [hostname, zustand, protokoll], 'a')
 
 
 if __name__ == "__main__":
     freeze_support()
     write(csv_file, table_head, 'w')
-    with Pool() as p:   # or ThreadPool()
+    with ThreadPool() as p:
         p.starmap(main, list((x,y) for x in range(1,5) for y in range(1,7)))
